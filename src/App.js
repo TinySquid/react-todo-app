b5aacf6cbed684ffa0574cf2814fc5693d5fc590
Michael Nunes <miketnunes@gmail.com>
2019-10-12 13:08:04 -0700

    Add todo functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import styled from 'styled-components';
 
 function App() {
+  //Todo state & initial todos
+  const [todos, setTodos] = useState([
+    {
+      content: 'Example todo #1',
+      isDone: false
+    },
+    {
+      content: 'Example todo #2',
+      isDone: false
+    },
+    {
+      content: 'Example todo #3',
+      isDone: false
+    },
+  ]);
+
+  const addTodo = content => {
+    console.log('add todo:', content);
+  }
+
+  const toggleTodoState = todoIndex => {
+
+  }
+
+  const deleteTodo = todoIndex => {
+
+  }
+
   return (
     <div>
-
     </div>
   );
 }

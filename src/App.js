14fdbd9beab078acaad5c6323d368a7d4625856e
Michael Nunes <miketnunes@gmail.com>
2019-10-12 14:09:41 -0700

    Add style components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Todo from './components/Todo';
-// import styled from 'styled-components';
+
+const AppRoot = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-content: center;
+`
+
+const TodoHeader = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+  color: whitesmoke;
+  letter-spacing: 0.1rem;
+	background-color: rgb(17, 109, 170, 0.8);
+  box-shadow: 0px -2px 3px 0px rgba(0,0,0,0.7);
+  margin-top: 5%;
+  padding-left: .8%;
+  width: 40%;
+`
+
+const TodoList = styled.div`
+  width: 40%;
+  padding: 1%;
+  box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.7);
+  background-color: rgb(42, 42, 42);
+`
 
 function App() {
   //Todo state & initial todos
@@ -32,13 +58,17 @@ function App() {
   }
 
   return (
-    <div>
-      <div className="todo-list">
+    <AppRoot>
+      <TodoHeader>
+        TodoList
+      </TodoHeader>
+      <TodoList>
+        {/* Map over todos and add a todo for each entry in state */}
         {todos.map((todo, idx) => (
           <Todo key={idx} todoIndex={idx} todo={todo} toggleTodoState={toggleTodoState} deleteTodo={deleteTodo} />
         ))}
-      </div>
-    </div>
+      </TodoList>
+    </AppRoot>
   );
 }
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const TodoItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const TodoButtons = styled.div`
+
+`
 
 export default function Todo({ todo, todoIndex, toggleTodoState, deleteTodo }) {
   return (
-    <div className="todo">
+    <TodoItem>
       {todo.content}
-      <div className="todo-buttons">
-        <button>Mark Complete</button>
-        <button>X</button>
-      </div>
-    </div>
+      <TodoButtons>
+        <button onClick={() => toggleTodoState(todoIndex)}>Mark Complete</button>
+        <button onClick={() => deleteTodo(todoIndex)}>X</button>
+      </TodoButtons>
+    </TodoItem>
   )
 }
+
+
